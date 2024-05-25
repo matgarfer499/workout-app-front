a193a98a2aa74857b828de22168dc90ab4af6e86
matiJGF14 <mjgarcia@bettergy.es>
2024-05-25 15:58:41 +0200

    feat: icons that will be used in the project

---
+++ src/app/lib/icons.js
@@ -0,0 +1,8 @@
+const Circle = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} {...props}>
+        <path
+            fill="currentColor"
+            d="M12 2A10 10 0 0 0 2 12a10 10 0 0 0 10 10 10 10 0 0 0 10-10A10 10 0 0 0 12 2"
+        />
+    </svg>
+)
